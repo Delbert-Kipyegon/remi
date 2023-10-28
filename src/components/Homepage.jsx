1fdf0fdf89abebf4158194a074436f317fd15249
Delbert-Kipyegon <delbertkip@gmail.com>
2023-10-28 15:15:59 +0300

    init

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+      <div className="nav"><h1>Nav goes here</h1></div>
+      <div className="cta"><h1>CTA + Image goes here</h1></div>
+      <div className="cards"><h1>6 Cards go here</h1></div>
+      <div className="short-content"><h1>Short content goes here</h1></div>
+      <div className="Subsribe"><h1>Subscribe to Mag </h1></div>
+      <div className="footer"><h1>Footer goes here</h1></div>
+    </div>
+  )
+}
+
+export default Homepage
