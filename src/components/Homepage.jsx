593cc6f53c7f65f8e8b8fd9995da4759a15bce32
Delbert-Kipyegon <delbertkip@gmail.com>
2023-11-01 20:20:03 +0300

    landing-page phase 1

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Nav from './Nav'
+import CTA from './CTA'
 
 function Homepage() {
   return (
     <div>
-      <div className="nav"><h1>Nav goes here</h1></div>
-      <div className="cta"><h1>CTA + Image goes here</h1></div>
+      <Nav />
+      <CTA />
       <div className="cards"><h1>6 Cards go here</h1></div>
       <div className="short-content"><h1>Short content goes here</h1></div>
       <div className="Subsribe"><h1>Subscribe to Mag </h1></div>
